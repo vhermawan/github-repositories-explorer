43aabf580827ef47baed2d9a979e079fc85d7ecb
vhermawan <vihermawan7@gmail.com>
2025-03-18 21:15:09 +0700

    chore: add example env and babel plugin compiler

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,11 @@ import path from "path"
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react({
+    babel: {
+      plugins: [["babel-plugin-react-compiler"]],
+    },
+  })],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
