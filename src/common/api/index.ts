8f69cd10196ba458e019a4e1581b5077d3a042f1
vhermawan <vihermawan7@gmail.com>
2025-03-20 15:24:32 +0700

    chore: revert axios func

--- src/common/api/index.ts
+++ src/common/api/index.ts
@@ -2,15 +2,11 @@ import axios from 'axios';
 
 export const API = {
   get : async function (endPoint: string) {    
-    try {
-      const response = await axios.get(import.meta.env.VITE_BASE_API_URL + endPoint);
+    return await axios.get(import.meta.env.VITE_BASE_API_URL + endPoint)
+    .then(response => {
       return response;
-    } catch (error: unknown) {
-      if (axios.isAxiosError(error)) {
-        throw error.response;
-      } else {
-        throw error;
-      }
-    }
+    }).catch(error => {
+      throw error.response;
+    });
   },
 }
